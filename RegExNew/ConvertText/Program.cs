404caaa696f3a44fb3332d347aa4684c3bf4dc6d
Istvan Turoczi <szolobor@gmail.com>
2020-01-06 11:12:04 +0100

    Convert text is ready

---
+++ RegExNew/ConvertText/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ConvertText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"C:\Users\User\Documents\tempDir\temp2\myfile.txt");
+            StreamWriter sw = new StreamWriter("myfile-utf7.txt", false, Encoding.UTF7);
+            sw.WriteLine(sr.ReadToEnd()); 
+            sw.Close();
+            sr.Close();
+        }
+    }
+}
